6e46887d91f85189ea1a9957b7be5e0afd236931
Kai Xu <xukai921110@gmail.com>
2018-09-04 09:39:14 +0100

    add codes

---
+++ src/Catsays.jl
@@ -0,0 +1,17 @@
+module Catsays
+
+function catsays(str)
+  l = length(str)
+  println("   ____")
+  println("  (.   \\           $(repeat("_", l + 2))")
+  println("    \\  |         /| $str |")
+  println("     \\ |___(\\--/)  $(repeat("-", l + 2))")
+  println("   __/    (  . . )")
+  println("  \"'._.    '-.O.'")
+  println("       '-.  \\ \"|\\")
+  println("          '.,,/'.,,")#mrf
+end
+
+export catsays
+
+end # module
